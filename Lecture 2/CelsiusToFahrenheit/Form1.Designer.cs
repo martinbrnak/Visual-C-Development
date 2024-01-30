d63d8d76ff49876ab82cc6721acdcd0288d77ede
martinbrnak <martin.brnak47@gmail.com>
2024-01-30 12:56:37 -0500

    Lecture 2

---
+++ Lecture 2/CelsiusToFahrenheit/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CelsiusToFahrenheit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.trackbarCels = new System.Windows.Forms.TrackBar();
+            this.txtWarning = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackbarCels)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(357, 37);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(166, 45);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fahrenheit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Celsius";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Location = new System.Drawing.Point(539, 195);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(154, 31);
+            this.txtFahrenheit.TabIndex = 3;
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(217, 195);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(140, 31);
+            this.txtCelsius.TabIndex = 4;
+            this.txtCelsius.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // trackbarCels
+            // 
+            this.trackbarCels.Location = new System.Drawing.Point(311, 300);
+            this.trackbarCels.Maximum = 200;
+            this.trackbarCels.Minimum = -100;
+            this.trackbarCels.Name = "trackbarCels";
+            this.trackbarCels.Size = new System.Drawing.Size(280, 69);
+            this.trackbarCels.TabIndex = 5;
+            this.trackbarCels.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // txtWarning
+            // 
+            this.txtWarning.AutoSize = true;
+            this.txtWarning.Location = new System.Drawing.Point(217, 233);
+            this.txtWarning.Name = "txtWarning";
+            this.txtWarning.Size = new System.Drawing.Size(0, 25);
+            this.txtWarning.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtWarning);
+            this.Controls.Add(this.trackbarCels);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackbarCels)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.TrackBar trackbarCels;
+        private System.Windows.Forms.Label txtWarning;
+    }
+}
+
---
+++ Lecture 2/CelsiusToFahrenheit/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusToFahrenheit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtWarning.Text = "";
+                double c = Double.Parse(txtCelsius.Text);
+                double f = CelsiusToFahrenheit(c);
+                txtFahrenheit.Text = f.ToString();
+            }
+            catch
+            {
+                txtWarning.Text = "Incorrect input";
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public double CelsiusToFahrenheit(double c)
+        {
+
+            return c * (9/5.0) + 32;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtWarning.Text = "";
+                double c = Double.Parse(txtCelsius.Text);
+                double f = CelsiusToFahrenheit(c);
+                txtFahrenheit.Text = f.ToString();
+            }
+            catch
+            {
+                txtWarning.Text = "Incorrect input";
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            txtCelsius.Text = trackbarCels.Value.ToString();
+        }
+    }
+}
